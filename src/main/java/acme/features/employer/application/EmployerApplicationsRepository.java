962063615a7e9f22225bf8c2f4ba8f8dee2d5a8e
ivanhr3 <ivaherrod1@alum.us.es>
2019-12-12 19:07:48 +0100

    Query changes
    Cambios en las querys para ordenar por ref, status y creationmoment.

--- src/main/java/acme/features/employer/application/EmployerApplicationsRepository.java
+++ src/main/java/acme/features/employer/application/EmployerApplicationsRepository.java
@@ -16,4 +16,13 @@ public interface EmployerApplicationsRepository extends AbstractRepository {
 	@Query("select a from Application a where a.id = ?1")
 	Application findOneById(int id);
 
+	@Query("select a from Application a where a.job.employer.id = ?1 order by a.ref asc")
+	Collection<Application> findManyByReference(int EmployerId);
+
+	@Query("select a from Application a where a.job.employer.id = ?1 order by a.status asc")
+	Collection<Application> findManyByStatus(int EmployerId);
+
+	@Query("select a from Application a where a.job.employer.id = ?1 order by a.creationmoment desc")
+	Collection<Application> findManyByCreationMoment(int EmployerId);
+
 }
